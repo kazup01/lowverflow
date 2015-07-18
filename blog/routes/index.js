6ea54cd2cb86638e87a27c475b70808e63000a32
YOKOMIZO, Kazumasa <Kazumasa@yokomizohajimeshou-no-MacBook-Pro.local>
2015-07-19 00:19:03 +0900

    connect to database

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
+var User = models.User;
+var Question = models.Question;
+var Answer = models.Answer;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
